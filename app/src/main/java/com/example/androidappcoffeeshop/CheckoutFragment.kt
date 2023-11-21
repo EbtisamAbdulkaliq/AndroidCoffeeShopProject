3c6324522486e784e3b1596d8a1366b113a7b4d6
unknown <eabdulk1@centennialcollege.ca>
2023-11-20 22:13:57 -0500

    Order ktln - summary fragment UPDATED

--- app/src/main/java/com/example/androidappcoffeeshop/CheckoutFragment.kt
+++ app/src/main/java/com/example/androidappcoffeeshop/CheckoutFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,25 +21,69 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class CheckoutFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+
+//    private fun isValidInput(): Boolean {
+//
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkout, container, false)
+        val view = inflater.inflate(R.layout.fragment_checkout, container, false)
+        val fullNumber = view.findViewById<EditText>(R.id.editText_fullName)
+        val phone = view.findViewById<EditText>(R.id.editText_phoneNumber)
+        val email = view.findViewById<EditText>(R.id.textEmailAddress)
+        val address = view.findViewById<EditText>(R.id.editText_address)
+        val city = view.findViewById<EditText>(R.id.editText_city)
+        val cardNumber = view.findViewById<EditText>(R.id.editText_cardNumber)
+        val cardHolderName = view.findViewById<EditText>(R.id.editText_cardholderName)
+        val cvv = view.findViewById<EditText>(R.id.editText_cvv)
+        val placeOrderButton = view.findViewById<Button>(R.id.button_placeOrder)
+        setupTimePicker()
+        placeOrderButton.setOnClickListener {
+            if (isValidInput()) {
+                navigateToNextPage()
+
+            } else {
+                Toast.makeText(activity, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return view
     }
 
-
+    private fun setupTimePicker() {
+        val timePicker = view?.findViewById<TimePicker>(R.id.timePicker)
+        timePicker?.setIs24HourView(true)
     }
+
+    private fun navigateToNextPage() {
+        //val intent = Intent(this, NextActivity::class.java)
+        // startActivity(intent)
+        // fragment next page navigation
+        val fragmentTransaction = fragmentManager?.beginTransaction()
+        fragmentTransaction?.replace(R.id.fragment_container, SummaryFragment())
+        fragmentTransaction?.addToBackStack(null)
+        fragmentTransaction?.commit()
+    }
+
+    private fun isValidInput(vararg field: EditText): Boolean {
+        for (i in field) {
+            if (i.text.toString().isEmpty()) {
+                return false
+            }
+        }
+        return true
+    }
+}
--- app/src/main/java/com/example/androidappcoffeeshop/OrdersFragment.kt
+++ app/src/main/java/com/example/androidappcoffeeshop/OrdersFragment.kt
@@ -15,7 +15,7 @@ import android.widget.Toast
 class OrdersFragment : Fragment() {
 //    private lateinit var cappuccinoButton: Button
 //    private lateinit var latteButton: Button
-private lateinit var espressoButton: Button
+//private lateinit var espressoButton: Button
 //    private lateinit var blondeRoastButton: Button
 //    private lateinit var coffeeSizeRadioGroup: RadioGroup
     private lateinit var continueButton: Button
@@ -31,67 +31,90 @@ private lateinit var espressoButton: Button
         val cappuccinoButton = view.findViewById<Button>(R.id.cappuccino)
         val darkRoastButton = view.findViewById<Button>(R.id.DarkRoast)
         val blondeRoastButton = view.findViewById<Button>(R.id.button_blondeRoast)
-        espressoButton = view.findViewById(R.id.Espressobtn)
+        val espressoButton = view.findViewById<Button>(R.id.Espressobtn)
         continueButton = view.findViewById(R.id.continueButton)
-       // cappuccinoButton = view.findViewById(R.id.cappuccino)
-         extrasCheckboxes = listOf(
+        // cappuccinoButton = view.findViewById(R.id.cappuccino)
+        extrasCheckboxes = listOf(
             view.findViewById(R.id.checkbox_cream),
             view.findViewById(R.id.checkbox_whole_milk),
             view.findViewById(R.id.checkbox_non_fat_milk),
             view.findViewById(R.id.checkbox_extra_shot),
         )
-     //   val creamcheckbox = view.findViewById<CheckBox>(R.id.checkbox_cream)
-       // val WoleMilkcheckBox = view.findViewById<CheckBox>(R.id.checkbox_whole_milk)
-      //  val nonFatcheckBox = view.findViewById<CheckBox>(R.id.checkbox_non_fat_milk)
+        //   val creamcheckbox = view.findViewById<CheckBox>(R.id.checkbox_cream)
+        // val WoleMilkcheckBox = view.findViewById<CheckBox>(R.id.checkbox_whole_milk)
+        //  val nonFatcheckBox = view.findViewById<CheckBox>(R.id.checkbox_non_fat_milk)
 
-      //  val espressoButton = view.findViewById<Button>(R.id.Espressobtn)
-
-
-
-        //coffee type bottun
-        cappuccinoButton.setOnClickListener(View.OnClickListener {
-
-       Toast.makeText(requireContext(), "Cappuccino", Toast.LENGTH_SHORT).show()
-       //  }
-
-          darkRoastButton.setOnClickListener {
-        Toast.makeText(requireContext(), "darkRoast", Toast.LENGTH_SHORT).show()
-         }
-//
-         blondeRoastButton.setOnClickListener {
-
-      Toast.makeText(requireContext(), "Latte", Toast.LENGTH_SHORT).show()
-
-        // }
-         espressoButton.setOnClickListener(View.OnClickListener {
-       Toast.makeText(requireContext(), "Espresso", Toast.LENGTH_SHORT).show()
-
-         }
+        //  val espressoButton = view.findViewById<Button>(R.id.Espressobtn)
 
 
+        //coffee type bottuns
+        //list
+        val coffeeTypeButton =
+            listOf(cappuccinoButton, darkRoastButton, blondeRoastButton, espressoButton)
+        coffeeTypeButton.forEach { button ->
+            button.setOnClickListener {
+                Toast.makeText(requireContext(), button.text, Toast.LENGTH_SHORT).show()
 
+
+            }
+        }
         //continue button
-                continueButton.setOnClickListener {
-             val selectedExtras = extrasCheckboxes.filter { it.isChecked }.map { it.text.toString() }
+//                 continueButton.setOnClickListener {
+//             val selectedExtras = extrasCheckboxes.filter { it.isChecked }.map { it.text.toString() }
+//  val message = if (selectedExtras.isNotEmpty()) {
+////             if (checkbox.isChecked) {
+////                    selectedExtras.add(checkbox.text.toString())
+////                }
+////            }
+//            val message = if (selectedExtras.isNotEmpty()){
+//                "You have selected: ${selectedExtras.joinToString(", ")}"
+//            } else {
+//                "You have selected no extras"
+//        }
+//Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+//
+//
+//          }
+//            return view
+//        })
+//    })}
 
-//             if (checkbox.isChecked) {
-//                    selectedExtras.add(checkbox.text.toString())
-//                }
-//            }
-            val message = if (selectedExtras.isNotEmpty()){
+        continueButton.setOnClickListener {
+            val selectedExtras = extrasCheckboxes.filter { it.isChecked }.map { it.text.toString() }
+            val message = if (selectedExtras.isNotEmpty()) {
                 "You have selected: ${selectedExtras.joinToString(", ")}"
             } else {
                 "You have selected no extras"
-        }
-Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+            }
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
 
-             val action = OrdersFragmentDirections.actionOrdersFragmentToSummaryFragment(selectedExtras.toTypedArray())
-             findNavController().navigate(action)
-         }
-
-             return view
-         }
+            // Navigation or additional actions here
         }
+
+        return view
+    }
+    // ... other class methods
+}
+//        cappuccinoButton.setOnClickListener(View.OnClickListener {
+//
+//       Toast.makeText(requireContext(), "Cappuccino", Toast.LENGTH_SHORT).show()
+//       //  }
+//
+//          darkRoastButton.setOnClickListener {
+//        Toast.makeText(requireContext(), "darkRoast", Toast.LENGTH_SHORT).show()
+//         }
+////
+//         blondeRoastButton.setOnClickListener {
+//
+//      Toast.makeText(requireContext(), "Latte", Toast.LENGTH_SHORT).show()
+//
+//        // }
+//         espressoButton.setOnClickListener(View.OnClickListener {
+//       Toast.makeText(requireContext(), "Espresso", Toast.LENGTH_SHORT).show()
+//
+//         })
+
+
 
 
 
