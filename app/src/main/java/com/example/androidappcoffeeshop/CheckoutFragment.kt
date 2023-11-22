8867d343463ac49630bd83c358e8715ff1358c65
unknown <eabdulk1@centennialcollege.ca>
2023-11-22 18:20:09 -0500

    checkout and summary.ktln UPDATED

--- app/src/main/java/com/example/androidappcoffeeshop/CheckoutFragment.kt
+++ app/src/main/java/com/example/androidappcoffeeshop/CheckoutFragment.kt
@@ -1,5 +1,3 @@
-package com.example.androidappcoffeeshop
-
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,19 +7,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TimePicker
 import android.widget.Toast
+import com.example.androidappcoffeeshop.R
+import com.example.androidappcoffeeshop.SummaryFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CheckoutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CheckoutFragment : Fragment() {
-
+    private lateinit var timePicker: TimePicker
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
@@ -35,11 +25,12 @@ class CheckoutFragment : Fragment() {
 //
 //    }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // Inflate the layouts
         val view = inflater.inflate(R.layout.fragment_checkout, container, false)
         val fullNumber = view.findViewById<EditText>(R.id.editText_fullName)
         val phone = view.findViewById<EditText>(R.id.editText_phoneNumber)
@@ -50,40 +41,80 @@ class CheckoutFragment : Fragment() {
         val cardHolderName = view.findViewById<EditText>(R.id.editText_cardholderName)
         val cvv = view.findViewById<EditText>(R.id.editText_cvv)
         val placeOrderButton = view.findViewById<Button>(R.id.button_placeOrder)
-        setupTimePicker()
+
+
+        timePicker = view.findViewById(R.id.timePicker)
+
+
+
         placeOrderButton.setOnClickListener {
-            if (isValidInput()) {
+            if (isValidInput(
+                    fullNumber,
+                    phone,
+                    email,
+                    address,
+                    city,
+                    cardNumber,
+                    cardHolderName,
+                    cvv
+                )
+            ) {
                 navigateToNextPage()
 
             } else {
                 Toast.makeText(activity, "Please fill in all the fields", Toast.LENGTH_SHORT).show()
             }
-
         }
+
         return view
     }
+    private fun isValidInput(vararg fields: EditText): Boolean {
+        return fields.all { it.text.isNotEmpty() == true }
 
-    private fun setupTimePicker() {
-        val timePicker = view?.findViewById<TimePicker>(R.id.timePicker)
-        timePicker?.setIs24HourView(true)
     }
 
-    private fun navigateToNextPage() {
-        //val intent = Intent(this, NextActivity::class.java)
-        // startActivity(intent)
-        // fragment next page navigation
-        val fragmentTransaction = fragmentManager?.beginTransaction()
-        fragmentTransaction?.replace(R.id.fragment_container, SummaryFragment())
-        fragmentTransaction?.addToBackStack(null)
-        fragmentTransaction?.commit()
+//    fun timepicker() {
+//            val timePicker = view.findViewById<TimePicker>(R.id.timePicker)
+//            val hour = timePicker.hour
+//            val minute = timePicker.minute
+//            val time = "$hour:$minute"
+//            Toast.makeText(activity, time, Toast.LENGTH_SHORT).show()
+//        }
+//        return view
+//    }
+
+
+
+    fun navigateToNextPage() {
+        val summaryFragment = SummaryFragment()
+
+        // Pass data to the summary fragment
+        val bundle = Bundle()
+        bundle.putString("PICKUP_TIME", getTimeFromPicker())
+        bundle.putString("ORDER_DETAILS", "Your order details here") // Replace with your logic
+        summaryFragment.arguments = bundle
+
+        val fragmentTransaction = parentFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, summaryFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 
-    private fun isValidInput(vararg field: EditText): Boolean {
-        for (i in field) {
-            if (i.text.toString().isEmpty()) {
-                return false
-            }
-        }
-        return true
+    private fun getTimeFromPicker(): String {
+        val hour = timePicker.hour
+        val minute = timePicker.minute
+        return "$hour:$minute"
     }
+
+
+
 }
+
+
+//        private fun navigateToNextPage() {
+//            //val intent = Intent(this, NextActivity::class.java)
+//            // startActivity(intent)
+//            // fragment next page navigation
+//
+
+
