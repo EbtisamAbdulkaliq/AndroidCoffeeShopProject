012da6eab174c5c512238ebfe3970d09683fdaf5
unknown <eabdulk1@centennialcollege.ca>
2023-11-24 02:28:42 -0500

    unknown error

--- app/src/main/java/com/example/androidappcoffeeshop/SummaryFragment.kt
+++ app/src/main/java/com/example/androidappcoffeeshop/SummaryFragment.kt
@@ -20,15 +20,12 @@ class SummaryFragment :
     ): View? {
         val view = inflater.inflate(R.layout.fragment_summary, container, false)
 
-        // Retrieve data from arguments
+
         val pickupTime = arguments?.getString("PICKUP_TIME")
         val orderDetails = arguments?.getString("ORDER_DETAILS")
-
-        // Find TextViews
         val textViewPickupTime = view.findViewById<TextView>(R.id.textViewPickupTime)
         val textViewOrderDetails = view.findViewById<TextView>(R.id.textViewOrderDetails)
 
-        // Set data to TextViews
         textViewPickupTime.text = "Pickup Time: $pickupTime"
         textViewOrderDetails.text = "Order Details: $orderDetails"
 
